da810161c1515772ebd101f428a74b2abbfeec2d
EricSSartorius <EricSSartorius@gmail.com>
2020-09-07 19:47:31 -0700

    Update styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,14 @@ import { ColumnLayout } from "../styles"
 export default function HomePage({ location }) {
   return (
     <Layout title="Home" description="" location={location}>
-      <div className="Wrapper">
-        <div className="Container">
+      <div className="wrapper">
+        <div className="Section">
           <div className="ColumnLayout" data-columns="2,2">
-            <div className="Copy">
+            <div>
               <h1 className="caption">I am caption text, check me out</h1>
-              <h2 className="title">I am a title</h2>
-              <div className="body">
-                <p>I am text</p>
-                <p className="small">I am small text</p>
-              </div>
+              <h2>I am a title</h2>
+              <p>I am text</p>
+              <p className="small">I am small text</p>
             </div>
             <div className="Card elevation-2">
               <p>cardo</p>
@@ -23,23 +21,21 @@ export default function HomePage({ location }) {
         </div>
       </div>
 
-      <div className="Wrapper">
-        <div className="Container">
-          <div className="Copy">
-            <h1>title</h1>
-            <p>text</p>
-          </div>
+      <div className="wrapper">
+        <div className="Section">
+          <h1>title</h1>
+          <p>text</p>
         </div>
       </div>
 
-      <div className="Wrapper">
-        <div className="Container">
+      <div className="wrapper">
+        <div className="Section">
           <ColumnLayout cols={[1, 1, 2]}>
-            <div className="Copy">
+            <div>
               <h1>title</h1>
               <p>text</p>
             </div>
-            <div className="Copy">
+            <div>
               <h1>title</h1>
               <p>text</p>
             </div>
