67fd01d0a88a4fa5948354438d800a287ac62e9e
Manticore <ryanbaugh9@gmail.com>
2021-02-11 18:25:45 -0500

    footer grid and styles

--- ui/src/layouts/Footer/index.tsx
+++ ui/src/layouts/Footer/index.tsx
@@ -8,26 +8,38 @@ export function Footer() {
   return (
     <footer className="footer">
       <div className="wrapper">
-        <div className="footer--grid">
+        <div className="footer footer--grid">
+          <div className="logo">
           <NavLink data-testid="logo-link" to="/" aria-label="home page">
             <Logo />
           </NavLink>
-          <nav>
+          </div>
+            <div className="about">
             <NavLink to="/about" aria-label="About page">
               About
             </NavLink>
+            </div>
+            <div className="qr-menus">
             <NavLink to="/qr-menus" aria-label="QR menus page">
               QR Menus
             </NavLink>
+            </div>
+            <div className="services">
             <NavLink to="/services" aria-label="Services page">
               Services
             </NavLink>
+            </div>
+            <div className="contact-us">
             <NavLink to="/contact" aria-label="Contact page">
               Contact Us
             </NavLink>
-          </nav>
-          <button className="cta cta--white-ghost cta--small">Sign Up</button>
-          <button className="cta cta--white-ghost cta--small">Sign In</button>
+            </div>
+          <div className="sign-up">
+          <button className="cta cta--white-ghost cta--small cta--footer">Sign Up</button>
+          </div>
+          <div className="sign-in">
+          <button className="cta cta--white-ghost cta--small cta--footer">Sign In</button>
+          </div>
         </div>
       </div>
       <div className="footer--bottom flex">
