0184da0cea7c4b3316acc64069c2b39c5200674a
EricSSartorius <EricSSartorius@gmail.com>
2021-02-13 18:11:21 -0700

    Add bg image to hero

--- ui/snowpack.config.js
+++ ui/snowpack.config.js
@@ -1,6 +1,6 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
-const httpProxy = require('http-proxy');
-const proxy = httpProxy.createServer({target: 'http://localhost:8000'});
+const httpProxy = require('http-proxy')
+const proxy = httpProxy.createServer({ target: 'http://localhost:8000' })
 module.exports = {
   mount: {
     public: { url: '/', static: true },
@@ -11,6 +11,7 @@ module.exports = {
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-sass',
     '@snowpack/plugin-typescript',
+    'snowpack-plugin-relative-css-urls',
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
