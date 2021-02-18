74b3c6761173e3d6a5bfe20a1f51059247cb336b
Manticore <ryanbaugh9@gmail.com>
2021-02-18 15:53:27 -0500

    footer dev

--- ui/src/layouts/Footer/index.tsx
+++ ui/src/layouts/Footer/index.tsx
@@ -7,30 +7,30 @@ import './footer.scss'
 export function Footer() {
   return (
     <footer className="footer">
-      <div className="padding padding--footer grid grid--footer">
+      <div className="footnav footMobile footstack">
         <div className="">
-          <NavLink className="footer--logo" data-testid="logo-link" to="/" aria-label="home page">
+          <NavLink className="" data-testid="logo-link" to="/" aria-label="home page">
             <Logo />
           </NavLink>
         </div>
-        <div className="grid grid--cols-3">
+        <div className="grid grid--cols-3 footMobile collapse">
             <NavLink className="text-white" to="/about" aria-label="About page">
               About
             </NavLink>
             <NavLink className="text-white" to="/qr-menus" aria-label="QR menus page">
               QR Menus
             </NavLink>
-            <button className="cta cta--white-ghost cta--small cta--footer">Sign Up</button>
+            <button className="cta cta--white-ghost cta--footer butorder">Sign Up</button>
             <NavLink className="text-white" to="/services" aria-label="Services page">
               Services
             </NavLink>
             <NavLink className="text-white" to="/contact" aria-label="Contact page">
               Contact Us
             </NavLink>
-          <button className="cta cta--white-ghost cta--small cta--footer">Sign In</button>
+          <button className="cta cta--white-ghost cta--footer">Sign In</button>
         </div>
       </div>
-      <div className="flex sub-footer padding--subFooter">
+      <div className="flex subFooter">
         <div>&copy; {new Date().getFullYear()} Pure Func LLC</div>
         <a className="text-white" href="mailto:contact@purefunc.io">Email: contact@purefunc.io</a>
       </div>
