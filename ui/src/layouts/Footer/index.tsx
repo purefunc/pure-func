a29586dd101fe93386ddedbfc049c8ed29727cb7
Manticore <ryanbaugh9@gmail.com>
2021-02-17 21:18:41 -0500

    WIP footer dev

--- ui/src/layouts/Footer/index.tsx
+++ ui/src/layouts/Footer/index.tsx
@@ -7,44 +7,32 @@ import './footer.scss'
 export function Footer() {
   return (
     <footer className="footer">
-      <div className="wrapper">
-        <div className="footer footer--grid">
-          <div className="logo">
-          <NavLink data-testid="logo-link" to="/" aria-label="home page">
+      <div className="padding padding--footer grid grid--footer">
+        <div className="">
+          <NavLink className="footer--logo" data-testid="logo-link" to="/" aria-label="home page">
             <Logo />
           </NavLink>
-          </div>
-            <div className="about">
-            <NavLink to="/about" aria-label="About page">
+        </div>
+        <div className="grid grid--cols-3">
+            <NavLink className="text-white" to="/about" aria-label="About page">
               About
             </NavLink>
-            </div>
-            <div className="qr-menus">
-            <NavLink to="/qr-menus" aria-label="QR menus page">
+            <NavLink className="text-white" to="/qr-menus" aria-label="QR menus page">
               QR Menus
             </NavLink>
-            </div>
-            <div className="services">
-            <NavLink to="/services" aria-label="Services page">
+            <button className="cta cta--white-ghost cta--small cta--footer">Sign Up</button>
+            <NavLink className="text-white" to="/services" aria-label="Services page">
               Services
             </NavLink>
-            </div>
-            <div className="contact-us">
-            <NavLink to="/contact" aria-label="Contact page">
+            <NavLink className="text-white" to="/contact" aria-label="Contact page">
               Contact Us
             </NavLink>
-            </div>
-          <div className="sign-up">
-          <button className="cta cta--white-ghost cta--small cta--footer">Sign Up</button>
-          </div>
-          <div className="sign-in">
           <button className="cta cta--white-ghost cta--small cta--footer">Sign In</button>
-          </div>
         </div>
       </div>
-      <div className="footer--bottom flex">
+      <div className="flex sub-footer padding--subFooter">
         <div>&copy; {new Date().getFullYear()} Pure Func LLC</div>
-        <a href="mailto:contact@purefunc.io">Email: contact@purefunc.io</a>
+        <a className="text-white" href="mailto:contact@purefunc.io">Email: contact@purefunc.io</a>
       </div>
     </footer>
   )
