4a6b883b475153aa7eda5f3b9f2056e9e47192b8
Boost <boostbm@gmail.com>
2021-02-18 16:03:35 -0800

    Updated Header

--- ui/src/layouts/Header/index.tsx
+++ ui/src/layouts/Header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { Logo } from 'components'
 import { Nav } from './Nav'
-
+import { Link } from 'react-router-dom'
 import './header.scss'
 
 export function Header() {
@@ -14,7 +14,31 @@ export function Header() {
             <Logo />
           </NavLink>
         </h1>
-        <Nav />
+        <nav>
+          <NavLink className="nav-link" to="/about" aria-label="About page">
+            About
+          </NavLink>
+          <NavLink className="nav-link" to="/services" aria-label="Services page">
+            Services
+          </NavLink>
+          <NavLink className="nav-link" to="/menus" aria-label="Digital Menus page">
+            Digital Menus
+          </NavLink>
+          {/* {!authState.isLoggedIn && <Login />}
+      {authState.isLoggedIn && (
+        <>
+        <button className="cta cta--cancel cta--small" onClick={logoff}>
+        Logoff
+        </button>
+        </>
+      )} */}
+        </nav>
+      </div>
+      <div>
+        <Link className="cta cta--white cta--small" to="/contact">
+          Contact Us
+        </Link>
+        <button className="cta cta--white-ghost cta--small">Sign Up</button>
       </div>
     </header>
   )
