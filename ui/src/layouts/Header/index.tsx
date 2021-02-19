6e9f1f1f5a84ee5d8e3b40354748b84a38791975
Boost <boostbm@gmail.com>
2021-02-18 16:04:37 -0800

    Updated Header

--- ui/src/layouts/Header/index.tsx
+++ ui/src/layouts/Header/index.tsx
@@ -1,30 +1,29 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import { Logo } from 'components'
-import { Nav } from './Nav'
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import './header.scss'
 
 export function Header() {
   return (
     <header className="header flex">
       <div className="header__inner flex wrapper">
-        <h1 className="margin-0 logo">
-          <NavLink data-testid="logo-link" to="/" aria-label="home page">
-            <Logo />
-          </NavLink>
-        </h1>
-        <nav>
-          <NavLink className="nav-link" to="/about" aria-label="About page">
-            About
-          </NavLink>
-          <NavLink className="nav-link" to="/services" aria-label="Services page">
-            Services
-          </NavLink>
-          <NavLink className="nav-link" to="/menus" aria-label="Digital Menus page">
-            Digital Menus
-          </NavLink>
-          {/* {!authState.isLoggedIn && <Login />}
+        <div>
+          <h1 className="margin-0 logo">
+            <NavLink data-testid="logo-link" to="/" aria-label="home page">
+              <Logo />
+            </NavLink>
+          </h1>
+          <nav>
+            <NavLink className="nav-link" to="/about" aria-label="About page">
+              About
+            </NavLink>
+            <NavLink className="nav-link" to="/services" aria-label="Services page">
+              Services
+            </NavLink>
+            <NavLink className="nav-link" to="/menus" aria-label="Digital Menus page">
+              Digital Menus
+            </NavLink>
+            {/* {!authState.isLoggedIn && <Login />}
       {authState.isLoggedIn && (
         <>
         <button className="cta cta--cancel cta--small" onClick={logoff}>
@@ -32,13 +31,14 @@ export function Header() {
         </button>
         </>
       )} */}
-        </nav>
-      </div>
-      <div>
-        <Link className="cta cta--white cta--small" to="/contact">
-          Contact Us
-        </Link>
-        <button className="cta cta--white-ghost cta--small">Sign Up</button>
+          </nav>
+        </div>
+        <div>
+          <Link className="cta cta--white cta--small" to="/contact">
+            Contact Us
+          </Link>
+          <button className="cta cta--white-ghost cta--small">Sign Up</button>
+        </div>
       </div>
     </header>
   )
