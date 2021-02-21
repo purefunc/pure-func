2af4fea8eaac905d9e7baa36fec6b3e214836737
EricSSartorius <EricSSartorius@gmail.com>
2021-02-20 17:50:28 -0700

    Remove react loadable

--- ui/src/layouts/MainLayout.tsx
+++ ui/src/layouts/MainLayout.tsx
@@ -1,38 +1,40 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Route, Switch } from 'react-router-dom'
-import {
-  AboutPage,
-  MenusPage,
-  MenusBetaPage,
-  MenusExamplesPage,
-  NotFoundPage,
-  DashboardLayout,
-  MenusPlansPage,
-  ContactPage,
-  ServicesPage,
-  SitesPage,
-} from './loadable'
-import HomePage from '../pages'
+
+const HomePage = lazy(() => import('../pages'))
+const AboutPage = lazy(() => import('../pages/about'))
+const MenusPage = lazy(() => import('../pages/menus'))
+const MenusBetaPage = lazy(() => import('../pages/menus/beta'))
+const MenusExamplesPage = lazy(() => import('../pages/menus/examples'))
+const NotFoundPage = lazy(() => import('../pages/404'))
+const MenusPlansPage = lazy(() => import('../pages/menus/plans'))
+const ContactPage = lazy(() => import('../pages/contact'))
+const ServicesPage = lazy(() => import('../pages/services'))
+const SitesPage = lazy(() => import('../pages/sites'))
+
+const DashboardLayout = lazy(() => import('./DashboardLayout'))
 
 const SiteLayout = () => {
   return (
     <main id="main">
-      <Switch>
-        {/* Static Pages */}
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/contact" component={ContactPage} />
-        <Route exact path="/services" component={ServicesPage} />
-        <Route exact path="/sites" component={SitesPage} />
-        <Route exact path="/menus" component={MenusPage} />
-        <Route exact path="/menus/beta" component={MenusBetaPage} />
-        <Route exact path="/menus/examples" component={MenusExamplesPage} />
-        <Route exact path="/menus/plans" component={MenusPlansPage} />
-        {/* Layouts */}
-        <Route path="/dashboard" component={DashboardLayout} />
-        {/* 404 Page */}
-        <Route component={NotFoundPage} />
-      </Switch>
+      <Suspense fallback={<div />}>
+        <Switch>
+          {/* Static Pages */}
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/contact" component={ContactPage} />
+          <Route exact path="/services" component={ServicesPage} />
+          <Route exact path="/sites" component={SitesPage} />
+          <Route exact path="/menus" component={MenusPage} />
+          <Route exact path="/menus/beta" component={MenusBetaPage} />
+          <Route exact path="/menus/examples" component={MenusExamplesPage} />
+          <Route exact path="/menus/plans" component={MenusPlansPage} />
+          {/* Layouts */}
+          <Route path="/dashboard" component={DashboardLayout} />
+          {/* 404 Page */}
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Suspense>
     </main>
   )
 }
