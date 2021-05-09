31d1e34ddbb7d7b537833c8009039903edd52e9a
EricSSartorius <EricSSartorius@gmail.com>
2021-05-09 12:09:41 -0400

    WIP Menu Form

--- server/models/menu.js
+++ server/models/menu.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const menuSchema = new Schema({
-  title: { 
+  title: {
     type: String,
     required: true,
   },
+  description: String,
   logo: String,
   bgImage: String,
-  categories: [String]
+  categories: [String],
 })
 
 menuSchema.index({ title: 1 })
