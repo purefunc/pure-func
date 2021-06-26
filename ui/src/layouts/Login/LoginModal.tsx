329c9f9361223484a77154ea1be2192d31837199
EricSSartorius <EricSSartorius@gmail.com>
2021-06-26 10:42:51 -0700

    Fix padding on login modal

--- ui/src/layouts/Login/LoginModal.tsx
+++ ui/src/layouts/Login/LoginModal.tsx
@@ -4,54 +4,46 @@ import { SignUpForm } from '../Login/Signup'
 import { PasswordResetForm } from '../Login/Reset'
 
 export const LoginModal = () => {
-    const [type, setType] = useState('login')
+  const [type, setType] = useState('login')
 
-    const toggleType = (val: string) => {
-        setType(val)
-    } 
+  const toggleType = (val: string) => {
+    setType(val)
+  }
 
   return (
     <>
-    {type === 'login' &&
-      <div>
-        <h3 className="text-center">Login</h3>
-        <LoginForm />
-        <div className="margin-top-small text-center">
-        <p>
-          Don't have an account? {' '}
-          <a onClick={() => toggleType('signup')}>
-            Register
-          </a>
-        </p>
-        <p>
-          Forgot your password? {' '}
-          <a onClick={() => toggleType('reset')}>
-            Reset
-          </a>
+      {type === 'login' && (
+        <div>
+          <h3 className="text-center margin-top-small">Login</h3>
+          <LoginForm />
+          <div className="margin-top-small text-center">
+            <p>
+              Don't have an account? <a onClick={() => toggleType('signup')}>Register</a>
+            </p>
+            <p>
+              Forgot your password? <a onClick={() => toggleType('reset')}>Reset</a>
+            </p>
+          </div>
+        </div>
+      )}
+      {type === 'signup' && (
+        <div>
+          <h3 className="text-center margin-top-small">Sign Up</h3>
+          <SignUpForm />
+          <p className="margin-top-small text-center">
+            Already have an account? <a onClick={() => toggleType('login')}>Sign in</a>
           </p>
         </div>
-      </div>}
-    {type === 'signup' &&
-      <div>
-        <h3 className="text-center">Sign Up</h3>
-        <SignUpForm />
-        <p className="margin-top-small text-center">Already have an account? {' '}
-          <a onClick={() => toggleType('login')}>
-            Sign in
-          </a>
-        </p>
-      </div>}
-    {type === 'reset' &&
-      <div>
-        <h3 className="text-center">Reset Password</h3>
+      )}
+      {type === 'reset' && (
+        <div>
+          <h3 className="text-center margin-top-small">Reset Password</h3>
           <PasswordResetForm />
           <p className="margin-top-small text-center">
-            Already have an account? {' '}
-            <a  onClick={() => toggleType('login')}>
-              Sign in
-            </a>
+            Already have an account? <a onClick={() => toggleType('login')}>Sign in</a>
           </p>
-      </div>}
+        </div>
+      )}
     </>
   )
 }
