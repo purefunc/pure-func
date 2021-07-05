6f5e7c9a11a6a3d621832d2b54bf599ea78bc8bb
EricSSartorius <EricSSartorius@gmail.com>
2021-07-05 12:49:58 -0700

    Update dashboard page redirect

--- ui/src/layouts/Header/index.tsx
+++ ui/src/layouts/Header/index.tsx
@@ -57,9 +57,9 @@ export function Header() {
         },
       })
       setIsCredsProcessed(true)
-      // Redirect to Dashboard if at home
+      // Redirect to Team Overview if at home
       if (!location.pathname.startsWith('/dashboard')) {
-        history.push('/dashboard')
+        history.push(`/dashboard/teams/${teamId}/overview`)
       }
     }
   }, [credsOnServer, isCredsProcessed, dispatch])
